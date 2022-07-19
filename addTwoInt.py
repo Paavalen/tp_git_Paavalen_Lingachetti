36bf13b3bc44f96dc80f843d704fae2d70e75a32
Paavalen <p.lingachetti@gmail.com>
2022-07-19 13:14:24 +0400

    Add feature to input values through the script
    When the user does not enter any arguments, the program will ask him to enter them.

--- addTwoInt.py
+++ addTwoInt.py
@@ -10,7 +10,14 @@ l=len(sys.argv)
 if l == 3 :
     a = int( sys.argv[1] )
     b = int( sys.argv[2] )
-    print(add(a,b))
 
+    print("Sum = ",add(a,b))
+
+elif l == 1:
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    
+    print("Sum = ",add(a,b))
+    
 else:
     print("ERROR: Enter 2 values only")

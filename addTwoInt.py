270192f0570f86a5823b917d87b392480362150b
Paavalen <p.lingachetti@gmail.com>
2022-07-19 12:50:49 +0400

    Add an input check
    Check that the correct number of arguments has been entered, otherwise return an error message.

--- addTwoInt.py
+++ addTwoInt.py
@@ -6,8 +6,11 @@ def add(a, b):
     s = a + b
     return s
 
+l=len(sys.argv)
+if l == 3 :
+    a = int( sys.argv[1] )
+    b = int( sys.argv[2] )
+    print(add(a,b))
 
-a = int( sys.argv[1] )
-b = int( sys.argv[2] )
-
-print(add(a,b))
+else:
+    print("ERROR: Enter 2 values only")
